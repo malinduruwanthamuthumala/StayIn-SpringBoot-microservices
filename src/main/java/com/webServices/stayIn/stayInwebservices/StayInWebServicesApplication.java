3419e4fdb559f35f20ab6774f479806769be796d
malinduR <malindu.r@tailwindsw.com>
2022-05-02 20:29:39 +0530

    first commit

---
+++ src/main/java/com/webServices/stayIn/stayInwebservices/StayInWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.webServices.stayIn.stayInwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayInWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StayInWebServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webServices/stayIn/stayInwebservices/StayInWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webServices.stayIn.stayInwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayInWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
