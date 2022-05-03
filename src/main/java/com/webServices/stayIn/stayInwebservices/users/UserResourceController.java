c8fd6a4c930ddeb0786674d1c012a3de9f225e90
malinduR <malindu.r@tailwindsw.com>
2022-05-03 21:41:28 +0530

    added hotel bean and DAO

---
+++ src/main/java/com/webServices/stayIn/stayInwebservices/users/UserResourceController.java
@@ -0,0 +1,8 @@
+package com.webServices.stayIn.stayInwebservices.users;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserResourceController {
+
+}
