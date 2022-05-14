837e4bc9cdcfda66f2155208427d3a828d95eecd
malinduR <malindu.r@tailwindsw.com>
2022-05-14 18:44:49 +0530

    added delete reqest for hotels

--- src/main/java/com/webServices/stayIn/stayInwebservices/Hotel/HotelController.java
+++ src/main/java/com/webServices/stayIn/stayInwebservices/Hotel/HotelController.java
@@ -1,13 +1,21 @@
 package com.webServices.stayIn.stayInwebservices.Hotel;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.webServices.stayIn.stayInwebservices.beans.Hotel;
+import com.webServices.stayIn.stayInwebservices.exception.UserNotFoundException;
 
 @RestController
 public class HotelController {
@@ -18,6 +26,33 @@ public class HotelController {
 	@GetMapping(path = "/Hotels")
 	public List<Hotel> getHotels(){
 		return hotelDAO.getHotels();
+	}
+	
+	@GetMapping(path = "/Hotels/{id}")
+	public Hotel findHotel(@PathVariable String id) throws NumberFormatException, UserNotFoundException {
+		return hotelDAO.findHotel(Integer.valueOf(id).intValue());
+	}
+	
+	@PostMapping("/Hotels")
+	public ResponseEntity<Hotel> saveHotel(@RequestBody Hotel hotel){
+		Hotel savedHotel = hotelDAO.Save(hotel);
+		
+		URI location =ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(savedHotel.getId()).toUri();
+		
+		return ResponseEntity.created(location).build();
+		
+		
+	}
+	
+	@DeleteMapping(path = "/Hotels/{id}")
+	public void deleteHotel(@PathVariable String id) throws UserNotFoundException  {
+		Hotel hotel = hotelDAO.deleteHotel(Integer.valueOf(id));
+		if (hotel == null) {
+			throw new UserNotFoundException();
+		}	
 	}
 
 }
--- src/main/java/com/webServices/stayIn/stayInwebservices/Hotel/HotelDAOService.java
+++ src/main/java/com/webServices/stayIn/stayInwebservices/Hotel/HotelDAOService.java
@@ -2,12 +2,14 @@ package com.webServices.stayIn.stayInwebservices.Hotel;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Component;
 
 import com.webServices.stayIn.stayInwebservices.beans.Hotel;
+import com.webServices.stayIn.stayInwebservices.exception.UserNotFoundException;
 
 @Component
 public class HotelDAOService {
@@ -17,6 +19,9 @@ public class HotelDAOService {
 	
 	static {
 		hotelList.add(new Hotel(1,"Savonra","Beach Hotel",23));
+		hotelList.add(new Hotel(2,"Savonra2","Beach Hotel",23));
+		hotelList.add(new Hotel(3,"Savonra3","Beach Hotel",23));
+		
 	}
 	
 	public List<Hotel> getHotels(){
@@ -24,18 +29,39 @@ public class HotelDAOService {
 	}
 	
 	public Hotel Save(Hotel hotel) {
-		if (hotel.getId() == null) {
+		if (hotel.getId()== null ) {
 			hotel.setId(++hotelCounter);
 			hotelList.add(hotel);
 		}
 		return hotel;
 	}
-	
-	public Hotel findHotel(int id) {
-	 Hotel requestedHotel = hotelList.stream()
-	  .filter(hotel -> hotel.getId().equals(Integer.valueOf(id)))
-	  .collect(Collectors.toList()).get(0);
-	 
-	 return requestedHotel;
+
+	public Hotel findHotel(int id) throws UserNotFoundException {
+
+		try {
+			Hotel requestedHotel = hotelList.stream().filter(hotel -> hotel.getId().equals(Integer.valueOf(id)))
+					.collect(Collectors.toList()).get(0);
+			return requestedHotel;
+
+		} catch (IndexOutOfBoundsException e) {
+			throw new UserNotFoundException();
+		}
+
 	}
+	
+	public Hotel deleteHotel(int id) {
+
+		Iterator<Hotel> hotelIterator = hotelList.listIterator();
+
+		while (hotelIterator.hasNext()) {
+			Hotel hotel = hotelIterator.next();
+			if (id == hotel.getId()) {
+				hotelIterator.remove();
+				return hotel;
+			}
+		}
+		return null;
+
+	}
+
 }
